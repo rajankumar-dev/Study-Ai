390e94fc583c3046ebcfee2c7c329e8894089625
rajankumar-dev <rk1620703@gmail.com>
2026-04-21 07:27:12 +0530

    feat: enhance profile picture management with upload and delete functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import fs from "fs";
+import path from "path";
 
 export const uploadProfilePic = async (req, res) => {
   try {
@@ -6,18 +8,52 @@ export const uploadProfilePic = async (req, res) => {
       return res.status(400).json({ message: "No file uploaded" });
     }
 
+    const user = await User.findById(req.user.id);
+
+    // STEP: Delete old image
+    if (user.profilePic) {
+      const oldPath = path.join(process.cwd(), user.profilePic);
+
+      fs.unlink(oldPath, (err) => {
+        if (err) console.log("Old file delete error:", err.message);
+      });
+    }
+
+    // New image
     const imageUrl = req.file.path;
 
-    const user = await User.findByIdAndUpdate(
-      req.user.id,
-      { profilePic: imageUrl },
-      { new: true },
-    );
+    user.profilePic = imageUrl;
+    await user.save();
 
     res.json({
       message: "Profile picture updated",
       profilePic: user.profilePic,
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteProfilePic = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user.profilePic) {
+      return res.status(400).json({ message: "No profile pic to delete" });
+    }
+
+    // delete from storage
+    const filePath = path.join(process.cwd(), user.profilePic);
+
+    fs.unlink(filePath, (err) => {
+      if (err) console.log("Delete error:", err.message);
+    });
+
+    // remove from DB
+    user.profilePic = null;
+    await user.save();
+
+    res.json({ message: "Profile picture deleted" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import upload from "../config/multer.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { uploadProfilePic } from "../controllers/user.controller.js";
+import {
+  uploadProfilePic,
+  deleteProfilePic,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +14,7 @@ router.post(
   upload.single("profilePic"),
   uploadProfilePic,
 );
+
+router.delete("/profile-pic", authMiddleware, deleteProfilePic);
 
 export default router;
