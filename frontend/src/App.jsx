819971b0cd177971ff1aa652f010318d72f4f4f4
rajankumar-dev <rk1620703@gmail.com>
2026-05-02 11:24:12 +0530

    feat: update routing in App component to include login and signup paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,25 @@ import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import CreateNote from "./pages/CreateNote";
 import Notes from "./pages/Notes";
+import Login from "./pages/Login";
+import Signup from "./pages/Sign";
 
 function App() {
+  const token = localStorage.getItem("token");
   return (
     <BrowserRouter>
       <Routes>
 
         {/* Layout Wrap */}
-        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route
+          path="/dashboard"
+          element={token ? <Layout><Home /></Layout> : <Login />}
+        />
         <Route path="/chat" element={<Layout><Chat /></Layout>} />
         <Route path="/create" element={<Layout><CreateNote /></Layout>} />
         <Route path="/notes" element={<Layout><Notes /></Layout>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
