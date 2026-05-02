9386bf4081df99e023726a7054f0a8c1946d4a1d
rajankumar-dev <rk1620703@gmail.com>
2026-05-02 09:47:30 +0530

    feat: add route for Notes page in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import CreateNote from "./pages/CreateNote";
+import Notes from "./pages/Notes";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/chat" element={<Layout><Chat /></Layout>} />
         <Route path="/create" element={<Layout><CreateNote /></Layout>} />
-
+        <Route path="/notes" element={<Layout><Notes /></Layout>} />
       </Routes>
     </BrowserRouter>
   );
